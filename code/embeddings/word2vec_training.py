28ec6f9e74fb7a7ce323adc4a4fb3acf84a4e4ce
Andrew <31720996+Andrews2017@users.noreply.github.com>
2020-10-13 17:56:57 +0800

    Update word2vec_training.py

--- code/embeddings/word2vec_training.py
+++ code/embeddings/word2vec_training.py
@@ -1,13 +1,6 @@
 from gensim.models import Word2Vec
 import pandas as pd
 
-stopset_kin = {'aba', 'abo', 'aha', 'aho', 'ari', 'ati', 'aya', 'ayo', 'ba', 'baba', 'babo', 'bari', 'be', 'bo', 'bose',
-           'bw', 'bwa', 'bwo', 'by', 'bya', 'byo', 'cy', 'cya', 'cyo', 'dr', 'hafi', 'ibi', 'ibyo', 'icyo', 'iki',
-           'imwe', 'iri', 'iyi', 'iyo', 'izi', 'izo', 'ka', 'ko', 'ku', 'kuri', 'kuva', 'kwa', 'maze', 'mu', 'muri',
-           'na', 'naho','nawe', 'ngo', 'ni', 'niba', 'nk', 'nka', 'no', 'nta', 'nuko', 'rero', 'rw', 'rwa', 'rwo', 'ry',
-           'rya','ubu', 'ubwo', 'uko', 'undi', 'uri', 'uwo', 'uyu', 'wa', 'wari', 'we', 'wo', 'ya', 'yabo', 'yari', 'ye',
-           'yo', 'yose', 'za', 'zo'}
-
 # load the data
 data_train = pd.read_csv('../data/KINNEWS/cleaned/train.csv')
 data_test = pd.read_csv('../data/KINNEWS/cleaned/test.csv')
@@ -22,10 +15,9 @@ data.whole_doc = data.whole_doc.str.replace('^https?:\/\/.*[\r\n]*', '')
 # Create the list of list format of the custom corpus for gensim modeling
 sent = [row.split(' ') for row in data['whole_doc'] if len(row)]
 sent = [[tok.lower() for tok in sub_sent if len(tok) != 0] for sub_sent in sent]
-sent = [[tok.lower() for tok in sub_sent if tok not in stopset_kin] for sub_sent in sent]
 
 # Training the model
-w2v_model = Word2Vec(sent, window=5, min_count=5, sg=1, hs=1, size=100)
+w2v_model = Word2Vec(sent, window=5, min_count=5, sg=1, hs=1, size=50)
 
 # Generate a list of words with their vectors to make the custom embeddings generation possible
 w2v_vectors = []
